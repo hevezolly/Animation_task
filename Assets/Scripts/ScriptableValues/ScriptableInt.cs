aa232ecaee12e662b677329e1f49d2b5b0040570
hevezolly <sevafinogenov@gmail.com>
2022-07-04 17:23:19 +0500

    folder restructuring

--- Assets/Scripts/ScriptableValues/ScriptableInt.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptable Values/Int")]
-public class ScriptableInt : ScriptableValue<int>
-{
-
-}
